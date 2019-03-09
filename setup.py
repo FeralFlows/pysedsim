e611cd4d244ddc6b2258b04a3404a8a5b02c22fd
FeralFlows <twild@umd.edu>
2019-03-09 08:05:32 -0500

    committing setup and installation files

---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+@author   Thomas B. Wild
+@email:   twild@umd.edu and tombernardwild@gmail.com
+@Project: PySedSim 1.0
+
+License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
+"""
+
+
+class VersionError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("Must have setuptools installed to run setup.py. Please install and try again.")
+    raise
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def get_requirements():
+    with open('requirements.txt') as f:
+        return f.read().split()
+
+
+setup(
+    name='PySedSim',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/FeralFlows/PySedSim',
+    license='BSD 2-Clause',
+    author='Thomas B. Wild; Abigail N. Birnbaum; Patrick M. Reed; Daniel P. Loucks',
+    author_email='twild@umd.edu; birnbaum.abigail@gmail.com; patrick.reed@cornell.edu; loucks@cornell.edu',
+    description='An Open Source Reservoir and Sediment Simulation Screening Framework for Identifying and Evaluating Dam Siting, Design, and Operation Alternatives',
+    long_description=readme(),
+    install_requires=get_requirements(),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+)
